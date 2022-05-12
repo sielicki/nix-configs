c380f2ec3ff71f394d62d3419adce2d3651579a8
Nicholas Sielicki <me@opensource.nslick.com>
2022-05-12 13:43:09 +0000

    updates

--- flake.nix
+++ flake.nix
@@ -4,20 +4,18 @@
   inputs = {
     configs.url             = "path:configs/";
     machines.url            = "path:machines/";
-    nixpkgs.url            = "github:nixos/nixpkgs/master";
-    #nixpkgs.url             = "github:nixos/nixpkgs/1fc7212a2c3992eedc6eedf498955c321ad81cc2";
-    #nixpkgs.url             = "github:sielicki/nixpkgs/master";
+    nixpkgs.url             = "github:nixos/nixpkgs/master";
+    nixpkgs-unstable.url    = "github:nixos/nixpkgs/nixos-unstable";
     darwin.url              = "github:LnL7/nix-darwin/master";
-    nixos-hardware.url     = "github:NixOS/nixos-hardware/master";
+    nixos-hardware.url      = "github:NixOS/nixos-hardware/master";
     home-manager.url        = "github:nix-community/home-manager/master";
     nix-doom-emacs.url      = "github:nix-community/nix-doom-emacs/master";
     emacs-overlay.url       = "github:nix-community/emacs-overlay/master";
     
     nixos-wsl.url             = "github:nix-community/nixos-wsl/main";
-    #nixos-wsl.url             = "github:nzbr/nixos-wsl/mount-fix";
 
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
   outputs = inputs@{
@@ -30,13 +28,13 @@
     nix-doom-emacs,
     nixos-hardware,
     nixos-wsl,
-    nixpkgs,
+    nixpkgs-unstable,
 
     ...
   }:
   let 
     system = "x86_64-linux";
-    pkgs = import nixpkgs {
+    pkgs = import nixpkgs-unstable {
       inherit system;
       config.allowUnfree = true;
       overlays = [ (import inputs.emacs-overlay) ];
@@ -44,7 +42,7 @@
   in
   {
     nixosConfigurations = {
-      dogdoor = nixpkgs.lib.nixosSystem {
+      dogdoor = nixpkgs-unstable.lib.nixosSystem {
         inherit system pkgs;
         modules = ([
 	  inputs.nixos-wsl.nixosModules.wsl
